37af1877d40d238f4baa004eac5c26c5dc79abf1
babahari19 <hariuottawa19@gmail.com>
2020-11-23 10:42:44 +0530

    Updates

---
+++ precache-manifest.903d70dcf63933c98a3c6e555157df68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "723dbec761d134c4c12d",
+    "url": "/expense-tracker/static/css/main.bff6f5f8.chunk.css"
+  },
+  {
+    "revision": "723dbec761d134c4c12d",
+    "url": "/expense-tracker/static/js/main.fda327d4.chunk.js"
+  },
+  {
+    "revision": "5baca6083ea1223d23cf",
+    "url": "/expense-tracker/static/js/runtime~main.ae18d0c9.js"
+  },
+  {
+    "revision": "ce9d27ff2ea8474768fd",
+    "url": "/expense-tracker/static/js/2.efe620a2.chunk.js"
+  },
+  {
+    "revision": "a66e9f2cbf48de269111631770076f8d",
+    "url": "/expense-tracker/index.html"
+  }
+];
